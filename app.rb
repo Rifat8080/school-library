00492cad3abd3eb5eb47bc87a81a7478cb21dfa3
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2023-11-13 22:08:01 +0600

    Made required changes

--- app.rb
+++ app.rb
@@ -2,101 +2,121 @@ require './person'
 require './student'
 require './teacher'
 require './classroom'
+require './rental'
 require './book'
 
-class App
-  def initialize(person_manager, book_manager, rental_manager)
-    @person_manager = person_manager
-    @book_manager = book_manager
-    @rental_manager = rental_manager
-  end
-
-  def execute_option(number)
-    case number
-    when 1
-      @book_manager.list_books
-    when 2
-      @person_manager.list_persons
-    when 3
-      create_person
-    when 4
-      create_book
-    when 5
-      create_rental(@book_manager.all_books, @person_manager.all_persons)
-    when 6
-      get_rental(@person_manager.all_persons)
-    else
-      puts 'Enter valid number'
-      main
-    end
-  end
-
-  def create_book
-    print 'Title: '
-    title = gets.chomp
-    print 'Author: '
-    author = gets.chomp
-    @book_manager.create_book(title, author)
-    puts 'Book created successfully'
-  end
-
-  def create_person
-    print 'Do you want to create student (1) or a teacher (2)? [Input number]: '
-    person_code = gets.chomp.to_i
+class InputManager
+  def self.get_age
     print 'Age: '
-    age = gets.chomp.to_i
-    print 'Name: '
-    name = gets.chomp
-    check_person_code(person_code, age, name)
-    puts 'Person created successfully'
+    gets.chomp.to_i
   end
 
-  def create_rental(all_books, all_persons)
+  def self.get_name
+    print 'Name: '
+    gets.chomp
+  end
+
+  def self.get_permission
+    print 'Has parent permission? [Y/N]: '
+    gets.chomp
+  end
+
+  def self.get_specialization
+    print 'Specialization: '
+    gets.chomp
+  end
+
+  def self.get_book_choice(books)
     puts 'Select a book from the following list by number'
-    all_books.each_with_index do |book, index|
+    books.each_with_index do |book, index|
       puts "#{index}) Title: #{book.title}, Author: #{book.author}"
     end
-    book_index = gets.chomp.to_i
+    gets.chomp.to_i
+  end
+
+  def self.get_person_choice(persons)
     puts 'Select person from the following list by number'
-    all_persons.each_with_index do |person, index|
+    persons.each_with_index do |person, index|
       person_type = person.instance_of?(Student) ? 'Student' : 'Teacher'
       puts "#{index})[#{person_type}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
-    person_index = gets.chomp.to_i
+    gets.chomp.to_i
+  end
+
+  def self.get_date
     print 'Date: '
-    date = gets.chomp
-    @rental_manager.create_rental(date, all_books[book_index], all_persons[person_index])
+    gets.chomp
+  end
+end
+
+class App
+  def initialize
+    @all_books = []
+    @all_persons = []
+  end
+
+  def create_book(title, author)
+    book = Book.new(title, author)
+    @all_books.push(book)
+    puts 'Book created successfully'
+  end
+
+  def create_person(person_code)
+    if person_code == 1
+      create_student
+    elsif person_code == 2
+      create_teacher
+    else
+      puts 'Enter valid number'
+      call(3)
+    end
+    puts 'Person created successfully'
+  end
+
+  def create_student
+    age = InputManager.get_age
+    name = InputManager.get_name
+    permission = InputManager.get_permission
+    permission_values = %w[n N]
+    person = Student.new(age, name, permission_values.include?(permission))
+    @all_persons.push(person)
+  end
+
+  def create_teacher
+    age = InputManager.get_age
+    name = InputManager.get_name
+    specialization = InputManager.get_specialization
+    person = Teacher.new(age, specialization, name)
+    @all_persons.push(person)
+  end
+
+  def list_books
+    puts @all_books.inspect
+    @all_books.each do |book|
+      puts "Title: \"#{book.title}\", Author: #{book.author}"
+    end
+  end
+
+  def list_persons
+    @all_persons.each do |person|
+      person_type = person.instance_of?(Student) ? 'Student' : 'Teacher'
+      puts "[#{person_type}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+  end
+
+  def create_rental
+    book_index = InputManager.get_book_choice(@all_books)
+    person_index = InputManager.get_person_choice(@all_persons)
+    date = InputManager.get_date
+    Rental.new(date, @all_books[book_index], @all_persons[person_index])
     puts 'Rental created successfully'
   end
 
-  def get_rental(all_persons)
-    print 'ID of person: '
-    id = gets.chomp.to_i
-    person = @rental_manager.get_rental(id, all_persons)
+  def get_rental(id)
+    person = @all_persons.find { |per| per.id == id }
     puts 'Rentals:'
     person.rentals.each do |rental|
       puts "Date: #{rental.date}, Book: \"#{rental.book.title}\" by #{rental.book.author}"
     end
-  end
-
-  def check_person_code(code, age, name)
-    if code == 1
-      create_student(age, name)
-    else
-      create_teacher(age, name)
-    end
-  end
-
-  def create_student(age, name)
-    print 'Has parent permission? [Y/N]: '
-    permission = gets.chomp
-    permission_values = %w[n N]
-    @person_manager.create_student(age, name, permission_values.include?(permission))
-  end
-
-  def create_teacher(age, name)
-    print 'Specialization: '
-    specialization = gets.chomp
-    @person_manager.create_teacher(age, name, specialization)
   end
 end
