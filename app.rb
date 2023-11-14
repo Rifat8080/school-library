8d014eb8d630d1a07bcc0e0d8eb1f1830683bdcb
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2023-11-14 23:35:02 +0600

    refactored code with preservation

--- app.rb
+++ app.rb
@@ -1,133 +1,163 @@
-require './person'
-require './student'
-require './teacher'
-require './classroom'
-require './book'
+require_relative 'book'
+require_relative 'person'
+require_relative 'student'
+require_relative 'teacher'
+require_relative 'rental'
+require_relative 'db_books'
+require_relative 'db_people'
+require_relative 'db_rentals'
 
-class InputHandler
-  def self.get_string(prompt)
-    print prompt
+class App
+  def initialize
+    @people = load_people
+    @books = load_books
+    @rentals = load_rentals
+  end
+
+  include BooksPersistence
+  include PeoplePersistence
+  include RentalsPersistence
+
+  def user_input(text)
+    print text
     gets.chomp
   end
 
-  def self.get_integer(prompt)
-    print prompt
-    gets.chomp.to_i
-  end
+  def run
+    puts 'Welcome to School Library App!'
+    loop do
+      puts '-' * 50
+      puts '
+          1- List all books
+          2- List all people
+          3- Create a new person
+          4- Create a new book entry
+          5- Create a new rental entry
+          6- List all rentals for a given person id
+          7- Quit'
 
-  def self.get_boolean(prompt)
-    print prompt
-    gets.chomp.downcase == 'y'
-  end
-end
+      input = user_input('Choose an option: ').to_i
 
-class PersonInputHandler
-  def self.create_person
-    type = InputHandler.get_integer('Do you want to create student (1) or a teacher (2)? [Input number]: ')
-    age = InputHandler.get_integer('Age: ')
-    name = InputHandler.get_string('Name: ')
+      break if input == 7
 
-    if type == 1
-      permission = InputHandler.get_boolean('Has parent permission? [Y/N]: ')
-      Student.new(age, name, parent_permission: permission) # Adjusted here
-    else
-      specialization = InputHandler.get_string('Specialization: ')
-      Teacher.new(age, name, specialization)
+      operation(input)
     end
-  end
-end
-
-class BookInputHandler
-  def self.create_book
-    title = InputHandler.get_string('Title: ')
-    author = InputHandler.get_string('Author: ')
-    Book.new(title, author)
-  end
-end
-
-class RentalInputHandler
-  def self.create_rental(all_books, all_persons)
-    puts 'Select a book from the following list by number'
-    all_books.each_with_index do |book, index|
-      puts "#{index}) Title: #{book.title}, Author: #{book.author}"
-    end
-    book_index = InputHandler.get_integer('')
-
-    puts 'Select person from the following list by number'
-    all_persons.each_with_index do |person, index|
-      person_type = person.instance_of?(Student) ? 'Student' : 'Teacher'
-      puts "#{index})[#{person_type}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-    end
-    person_index = InputHandler.get_integer('')
-
-    date = InputHandler.get_string('Date: ')
-    [all_books[book_index], all_persons[person_index], date]
+    store_books(@books)
+    store_people(@people)
+    store_rentals(@rentals)
   end
 
-  def self.rental_person_id
-    InputHandler.get_integer('ID of person: ')
-  end
-end
-
-class App
-  def initialize(person_manager, book_manager, rental_manager)
-    @person_manager = person_manager
-    @book_manager = book_manager
-    @rental_manager = rental_manager
-  end
-
-  def execute_option(number)
-    case number
+  def operation(input)
+    case input
     when 1
-      @book_manager.list_books
+      list_books
     when 2
-      @person_manager.list_persons
+      list_people
     when 3
       create_person
     when 4
       create_book
     when 5
-      create_rental(@book_manager.all_books, @person_manager.all_persons)
+      create_rental
     when 6
-      get_rental(@person_manager.all_persons)
+      list_rentals
     else
-      puts 'Enter valid number'
-      main
+      puts 'Please choose a valid option number'
+    end
+  end
+
+  def list_books
+    puts '-' * 50
+    if @books.empty?
+      puts 'The books list is empty'
+    else
+      puts 'Books list:'
+      @books.each_with_index do |book, index|
+        puts "#{index + 1}-[Book] Title: #{book.title} | Author: #{book.author}"
+      end
+    end
+  end
+
+  def list_people
+    puts '-' * 50
+    if @people.empty?
+      puts "The people\'s list is empty"
+    else
+      puts 'People list:'
+      @people.each_with_index do |per, i|
+        puts "#{i + 1}-[Teacher] ID: #{per.id} | Name: #{per.name} | Specialization: #{per.specialization} | Age: #{per.age}" if per.is_a?(Teacher)
+        puts "#{i + 1}-[Student] ID: #{per.id} | Name: #{per.name} | Age: #{per.age}" if per.is_a?(Student)
+      end
     end
   end
 
   def create_person
-    person = PersonInputHandler.create_person
-    if person.is_a?(Student)
-      student_permission = InputHandler.get_boolean('Has parent permission? [Y/N]: ')
-      person = Student.new(person.age, person.name, parent_permission: student_permission)
+    puts '-' * 50
+    option = user_input('Do you want to create a student (1) or a teacher (2)? [input the number]: ').to_i
+    name = user_input('Name: ')
+    age = user_input('Age: ')
+    case option
+    when 1
+      create_student(name, age)
+    when 2
+      create_teacher(name, age)
+    else
+      puts 'Please input a valid number (1 or 2)'
     end
+  end
 
-    @person_manager.add_person(person)
-    puts 'Person created successfully'
+  def create_student(name, age)
+    parent_permission = user_input("Has parent\'s persmission? [Y/N]: ")
+    parent_permission = true if parent_permission == ('y' || 'Y')
+    parent_permission = false if parent_permission == ('n' || 'N')
+    @people << Student.new(Random.rand(1..1000), age, 'class', name, parent_permission: parent_permission)
+    puts "Student (#{name}) has been created successfully"
+    store_people(@people)
+  end
+
+  def create_teacher(name, age)
+    specialization = user_input("Teacher\'s specialization: ")
+    @people << Teacher.new(Random.rand(1..1000), age, specialization, name)
+    puts "Teacher (#{name}) has been created successfully"
+    store_people(@people)
   end
 
   def create_book
-    book = BookInputHandler.create_book
-    @book_manager.add_book(book)
-    puts 'Book created successfully'
+    title = user_input("Book\'s title: ")
+    author = user_input("Book\'s author: ")
+    @books << Book.new(title, author, nil)
+    puts "Book (#{title} By #{author}) has been created and saved successfully"
+    store_books(@books)
   end
 
-  def create_rental(all_books, all_persons)
-    book, person, date = RentalInputHandler.create_rental(all_books, all_persons)
-    @rental_manager.create_rental(person.id, date, book, person)
-    puts 'Rental created successfully'
-  end
-
-  def get_rental(_all_persons)
-    id = RentalInputHandler.rental_person_id
-    rental = @rental_manager.get_rental(id)
-
-    if rental.nil?
-      puts 'Rental not found.'
+  def create_rental
+    if @books.empty?
+      puts 'Books list is empty, please create a book first'
+    elsif @people.empty?
+      puts "People\'s list is empty, please create a person first"
     else
-      puts 'Rentals:'
-      puts "Date: #{rental.date}, Book: \"#{rental.book.title}\" by #{rental.book.author}"
+      list_books
+      book_number = user_input('Select a book from the following list by number: ').to_i
+      list_people
+      person_number = user_input('Select a person from the following list by number: ').to_i
+      date = user_input('Date: ')
+      book = "#{@books[book_number - 1].title} By #{@books[book_number - 1].author}"
+      @rentals << Rental.new(date, book, @people[person_number - 1].id)
+      store_rentals(@rentals)
+      puts 'Rental has been created successfully'
+    end
+  end
+
+  def list_rentals
+    list_people
+    input_id = user_input("Person\'s ID: ").to_i
+    selected_person = @rentals.select { |rental| rental.person == input_id }
+    if selected_person.empty?
+      puts "No rentals are found for (#{input_id})"
+    else
+      selected_person.each do |rental|
+        puts "Date: #{rental.date} | Book: #{rental.book}"
+      end
     end
   end
 end
